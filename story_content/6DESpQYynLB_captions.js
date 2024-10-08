d9297ff28a85ab62c9dc144444f8ba608f8bf98a
Omar Assioua <assioua243@gmail.com>
2024-10-08 08:15:33 +0100

    Summary

---
+++ story_content/6DESpQYynLB_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.92.33225.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.101%0D%0AArtuze%20remains%20rooted%20to%20the%20spot%20as%20danger%20looms%20all%20around%20her%20in%20the%20dense%20jungle.%0D%0A%0D%0A"}')
